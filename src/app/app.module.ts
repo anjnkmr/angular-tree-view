7cc790f3b9a7fa4298fcfbeaf9c3b4f99bec62b7
Anjan Kumar <kumar.rakuditi@gmail.com>
2019-02-21 18:26:34 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TreeTableComponent } from './components/tree-table/tree-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
